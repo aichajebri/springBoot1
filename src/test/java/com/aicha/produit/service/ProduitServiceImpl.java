7aecbe9da855afc426be6ab7ceb625aaf626b98c
aichajebri <77939143+aichajebri@users.noreply.github.com>
2022-04-06 23:47:51 +0100

    1st springboot

---
+++ src/test/java/com/aicha/produit/service/ProduitServiceImpl.java
@@ -0,0 +1,87 @@
+package com.aicha.produit.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.aicha.produit.entities.Produit;
+import com.aicha.produit.repos.ProduitRepository;
+
+public class ProduitServiceImpl implements ProduitService {
+@Autowired
+ProduitRepository produitRepository;
+@Override
+public Produit saveProduit1(Produit p) {
+	public class ProduitServiceImpl implements ProduitService {
+		@Autowired
+		ProduitRepository produitRepository;
+		@Override
+		public Produit saveProduit(Produit p) {
+}
+	public void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public Produit updateProduit(Produit p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void deleteProduit(Produit p) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void deleteProduitById(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public Produit getProduit(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public List<Produit> getAllProduits() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public Produit saveProduit1(Produit p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
+
+@Override
+public Produit saveProduit(Produit p) {
+	// TODO Auto-generated method stub
+	return null;
+}
+@Override
+public Produit updateProduit(Produit p) {
+	// TODO Auto-generated method stub
+	return null;
+}
+@Override
+public void deleteProduit(Produit p) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void deleteProduitById(Long id) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public Produit getProduit(Long id) {
+	// TODO Auto-generated method stub
+	return null;
+}
+@Override
+public List<Produit> getAllProduits() {
+	// TODO Auto-generated method stub
+	return null;
+}
---
+++ src/test/java/com/aicha/produit/service/ProduitServiceImplTest.java
@@ -0,0 +1,14 @@
+package com.aicha.produit.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProduitServiceImplTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
